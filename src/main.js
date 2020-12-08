9b0d8c6ea68146c2f03db12fc2abdb48554dd334
gym <8266493+xiaoguoaichiyu@user.noreply.gitee.com>
2020-12-08 11:33:03 +0800

    init shopg

---
+++ src/main.js
@@ -0,0 +1,10 @@
+/* 入口 */
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})
